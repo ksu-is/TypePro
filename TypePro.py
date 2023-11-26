4544d7d7aee3996326501da8222b401add74e03a
cvilfort1 <111465481+cvilfort1@users.noreply.github.com>
2023-11-26 17:23:22 -0500

    Update TypePro.py

--- TypePro.py
+++ TypePro.py
@@ -8,3 +8,8 @@ class TypeSpeedGUI:
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title("Typing Speed Application")
+        self.root.geometry("800x600")
+
+        self.texts = open("texts.txt", "r").read().split("\n")
+        
+        self.sample_label = tk.Label(self.root, text="Sample Text")
