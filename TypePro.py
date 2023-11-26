8e552da9f9f307b3ed04c211e4ba16dcf9b84a7c
cvilfort1 <111465481+cvilfort1@users.noreply.github.com>
2023-11-26 17:09:41 -0500

    Add files via upload

---
+++ TypePro.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import time
+import threading
+import random
+
+class TypeSpeedGUI:
+
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title("Typing Speed Application")
